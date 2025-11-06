4aee5010bcb203f1686a7766a5e375770ae7972c
Zhuykov <rsfinxr@gmail.com>
2025-11-06 13:59:55 +0400

    сортировка пзырьком

---
+++ src/ISortStrategy.java
@@ -0,0 +1,9 @@
+package src;
+
+public interface ISortStrategy {
+    /**
+     * Метод для сортировки массива целых чисел.
+     * @param array Массив, который нужно отсортировать.
+     */
+    void Sort(int[] array);
+}
