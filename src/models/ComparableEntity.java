3bd778b5dbc385f638acabe14988e91dacc51bac
Zhuykov <rsfinxr@gmail.com>
2025-11-11 00:50:40 +0400

    исправлен поиск

--- src/models/ComparableEntity.java
+++ src/models/ComparableEntity.java
@@ -1,8 +1,10 @@
 package src.models;
 
 public interface ComparableEntity extends Comparable<ComparableEntity> {
-    @Override
-    default int compareTo(ComparableEntity other) {
-        return EntityComparator.DEFAULT.compare(this, other);
-    }
+	int getId();
+
+	@Override
+	default int compareTo(ComparableEntity other) {
+		return EntityComparator.DEFAULT.compare(this, other);
+	}
 }
