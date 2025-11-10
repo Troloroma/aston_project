42cc09177acf5aa2ca50ba66eb30c78ca2f431dc
Zhuykov <rsfinxr@gmail.com>
2025-11-10 14:11:08 +0400

    Поправил указание пакета models

--- src/models/PersonComparator.java
+++ src/models/PersonComparator.java
@@ -1,4 +1,4 @@
-package models;
+package src.models;
 
 import java.util.Comparator;
 import java.util.List;
@@ -11,46 +11,46 @@ import java.util.Map;
 
 public class PersonComparator implements Comparator<Person> {
 
-    /***
-     *  Компараторы для Teacher и Student.
-     *  Приоритет сравнений у Teacher: lastName, firstName, id, subject, experience
-     *  Приоритет сравнений у Student: lastName, firstName, id, grade, age
-     */
-    private final Map<Class<?>, Comparator<?>> comparators = Map.of(
-            Teacher.class, Comparator.comparing(Teacher::getLastName)
-                    .thenComparing(Teacher::getFirstName)
-                    .thenComparingInt(Teacher::getId)
-                    .thenComparing(Teacher::getSubject)
-                    .thenComparingInt(Teacher::getExperience),
+	/***
+	 *  Компараторы для Teacher и Student.
+	 *  Приоритет сравнений у Teacher: lastName, firstName, id, subject, experience
+	 *  Приоритет сравнений у Student: lastName, firstName, id, grade, age
+	 */
+	private final Map<Class<?>, Comparator<?>> comparators = Map.of(
+		Teacher.class, Comparator.comparing(Teacher::getLastName)
+			.thenComparing(Teacher::getFirstName)
+			.thenComparingInt(Teacher::getId)
+			.thenComparing(Teacher::getSubject)
+			.thenComparingInt(Teacher::getExperience),
 
-            Student.class, Comparator.comparing(Student::getLastName)
-                    .thenComparing(Student::getFirstName)
-                    .thenComparingInt(Student::getId)
-                    .thenComparingInt(Student::getGrade)
-                    .thenComparingInt(Student::getAge)
-    );
+		Student.class, Comparator.comparing(Student::getLastName)
+			.thenComparing(Student::getFirstName)
+			.thenComparingInt(Student::getId)
+			.thenComparingInt(Student::getGrade)
+			.thenComparingInt(Student::getAge)
+	);
 
-    private final List<Class<?>> typeOrder = List.of(
-            Teacher.class,  // сначала учителя
-            Student.class   // потом студенты
-    );
+	private final List<Class<?>> typeOrder = List.of(
+		Teacher.class, // сначала учителя
+		Student.class // потом студенты
+	);
 
-    @Override
-    public int compare(Person p1, Person p2) {
-        // Сортировка по типу (Teacher → Student)
-        int typeCompare = Integer.compare(
-                typeOrder.indexOf(p1.getClass()),
-                typeOrder.indexOf(p2.getClass())
-        );
-        if (typeCompare != 0) return typeCompare;
+	@Override
+	public int compare(Person p1, Person p2) {
+		// Сортировка по типу (Teacher → Student)
+		int typeCompare = Integer.compare(
+			typeOrder.indexOf(p1.getClass()),
+			typeOrder.indexOf(p2.getClass())
+		);
+		if (typeCompare != 0) return typeCompare;
 
-        // Если одного типа - выбирается компаратор из мапы
-        Comparator<Person> cmp = getComparatorFor(p1.getClass());
-        return cmp.compare(p1, p2);
-    }
+		// Если одного типа - выбирается компаратор из мапы
+		Comparator<Person> cmp = getComparatorFor(p1.getClass());
+		return cmp.compare(p1, p2);
+	}
 
-    private <T extends Person> Comparator<T> getComparatorFor(Class<?> clazz) {
-        //noinspection unchecked
-        return (Comparator<T>) comparators.getOrDefault(clazz, Comparator.comparing(Person::getLastName));
-    }
+	private <T extends Person> Comparator<T> getComparatorFor(Class<?> clazz) {
+		//noinspection unchecked
+		return (Comparator<T>) comparators.getOrDefault(clazz, Comparator.comparing(Person::getLastName));
+	}
 }
--- src/models/Student.java
+++ src/models/Student.java
@@ -1,4 +1,4 @@
-package models;
+package src.models;
 
 /***
  * Use example:
@@ -11,98 +11,98 @@ package models;
  *                 .build());
  */
 public class Student implements Person {
-    private final String firstName;
-    private final String lastName;
-    private final int age;
-    private final int grade; // from 1 to 11
-    private final int id;
+	private final String firstName;
+	private final String lastName;
+	private final int age;
+	private final int grade; // from 1 to 11
+	private final int id;
 
-    public Student(StudentBuilder builder) {
-        if (builder == null) throw new IllegalArgumentException("Builder cannot be null");
-        this.firstName = builder.firstName;
-        this.lastName = builder.lastName;
-        this.age = builder.age;
-        this.grade = builder.grade;
-        this.id = builder.id;
-    }
+	public Student(StudentBuilder builder) {
+		if (builder == null) throw new IllegalArgumentException("Builder cannot be null");
+		this.firstName = builder.firstName;
+		this.lastName = builder.lastName;
+		this.age = builder.age;
+		this.grade = builder.grade;
+		this.id = builder.id;
+	}
 
-    @Override
-    public String getFirstName() {
-        return firstName;
-    }
+	@Override
+	public String getFirstName() {
+		return firstName;
+	}
 
-    @Override
-    public String getLastName() {
-        return lastName;
-    }
+	@Override
+	public String getLastName() {
+		return lastName;
+	}
 
-    @Override
-    public int getId() {
-        return 0;
-    }
+	@Override
+	public int getId() {
+		return id;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	public int getAge() {
+		return age;
+	}
 
-    public int getGrade() {
-        return grade;
-    }
+	public int getGrade() {
+		return grade;
+	}
 
-    @Override
-    public String toString() {
-        return "Student [firstName=" + firstName +
-                ", lastName=" + lastName +
-                ", age=" + age +
-                ", grade=" + grade +
-                ", id=" + id + "]";
-    }
+	@Override
+	public String toString() {
+		return "Student [firstName=" + firstName +
+				", lastName=" + lastName +
+				", age=" + age +
+				", grade=" + grade +
+				", id=" + id + "]";
+	}
 
-    public static class StudentBuilder {
-        private String firstName;
-        private String lastName;
-        private int age;
-        private int grade;
-        private int id;
+	public static class StudentBuilder {
+		private String firstName;
+		private String lastName;
+		private int age;
+		private int grade;
+		private int id;
 
-        public StudentBuilder firstName(String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
+		public StudentBuilder firstName(String firstName) {
+			this.firstName = firstName;
+			return this;
+		}
 
-        public StudentBuilder lastName(String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
+		public StudentBuilder lastName(String lastName) {
+			this.lastName = lastName;
+			return this;
+		}
 
-        public StudentBuilder age(int age) {
-            this.age = age;
-            return this;
-        }
+		public StudentBuilder age(int age) {
+			this.age = age;
+			return this;
+		}
 
-        public StudentBuilder grade(int grade) {
-            this.grade = grade;
-            return this;
-        }
+		public StudentBuilder grade(int grade) {
+			this.grade = grade;
+			return this;
+		}
 
-        public StudentBuilder id(int id) {
-            this.id = id;
-            return this;
-        }
+		public StudentBuilder id(int id) {
+			this.id = id;
+			return this;
+		}
 
-        public Student build() {
-            if (!validateStudent()) {
-                throw new IllegalArgumentException("Some of the fields are invalid");
-            }
-            return new Student(this);
-        }
+		public Student build() {
+			if (!validateStudent()) {
+				throw new IllegalArgumentException("Some of the fields are invalid");
+			}
+			return new Student(this);
+		}
 
-        private boolean validateStudent() {
-            return (firstName != null && !firstName.trim().isEmpty()
-                    && lastName != null && !lastName.trim().isEmpty()
-                    && age >= 0
-                    && grade > 0 && grade <= 11
-                    && id >= 0);
-        }
-    }
+		private boolean validateStudent() {
+			return (firstName != null && !firstName.trim().isEmpty()
+					&& lastName != null && !lastName.trim().isEmpty()
+					&& age >= 0
+					&& grade > 0 && grade <= 11
+					&& id >= 0);
+		}
+	}
 }
